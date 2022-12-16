c5d95c194ebaee3187ff1ab15aab56027b621c90
LYS <laiyishuai@qq.com>
2022-12-17 02:35:17 +0800

    modify-0005

---
+++ L0007_QGridLayout/GridLayout/mainwindow.cpp
@@ -0,0 +1,28 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    int iNum;//6
+
+    ui->setupUi(this);
+    iNum = 0;//7
+    m_pGridLayout = new QGridLayout(ui->centralWidget);//8
+    for(int i=0; i<3; i++)//9
+    {//10
+        for(int j=0; j<3; j++)//11
+        {//12
+            m_arrayBtn[iNum] = new QPushButton(QString("row:%1-column:%2--index=%3").arg(i).arg(j).arg(i*3+j),ui->centralWidget);//13
+            m_pGridLayout->addWidget(m_arrayBtn[iNum],i,j,1,1);//14
+            iNum++;//15
+        }//16
+    }//17
+    m_pGridLayout->setSpacing(0);//18
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
---
+++ L0007_QGridLayout/GridLayout/mainwindow.h
@@ -0,0 +1,28 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QGridLayout>//1
+#include <QPushButton>//2
+#include <array>//3
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+public:
+    QGridLayout* m_pGridLayout;//4
+    std::array<QPushButton*,9> m_arrayBtn;//5
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
